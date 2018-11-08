5eaf01d3f6f3b45fe80a7fc1979bb4ab1a3ce323
NissimBet <nissimbet123@outlook.com>
2018-11-08 09:45:06 -0600

    added subtitle component. finished contact page

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import { Container } from '../Container';
 import Map from './Map';
 import BusinessInfo from './BusinessInfo';
 import Form from './Form';
+import SubTitle from './../SubTitle';
+
+const Description = styled(SubTitle)`
+  width: 60%;
+  left: 15em;
+`;
 
 const Contact = () => (
-  <Container flex align="flex-start">
+  <Container flex align="flex-start" margin={[3,0]}>
+    <Description title="Get a quote" size={2.5}>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    </Description>
     <Form />
     <Map />
     <BusinessInfo />
---
+++ src/components/SubTitle/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+import { Container } from '../Container';
+import { Text } from '../Text';
+
+const TextGreenEdge = styled(Text)`
+  ::before {
+    content: "";
+    position: absolute;
+    height: 100%;
+    width: 0.4em;
+    left: 0;
+    top: 0;
+    background-color: ${({theme}) => theme.color.green};  
+  }
+`;
+
+const SubTitle = ({children, title, size = 1, ...props}) => (
+  <Container flex align="flex-start" width='auto' {...props}>
+    <Text bold size={size*3}>{title}</Text>
+    <TextGreenEdge size={size} padding={[0,1]} as={Container}>{children}</TextGreenEdge>
+  </Container>
+)
+
+SubTitle.PropTypes = {
+  title: PropTypes.string.isRequired,
+  size: PropTypes.number
+}
+
+export default SubTitle;
