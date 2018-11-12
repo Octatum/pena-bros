928d427fd68a1b3a14431f1fcb666d2f2275d4d3
NissimBet <nissimbet123@outlook.com>
2018-11-11 21:46:29 -0600

    changed review style

--- src/components/Home/Quote/index.jsx
+++ src/components/Home/Quote/index.jsx
@@ -3,30 +3,38 @@ import styled from 'styled-components';
 import { Container } from '../../Container';
 import { Text } from '../../Text';
 
-const Author = styled(Text)`
-  width: 20%;
+import inicial from './assets/comilla_inicial.png';
+import final from './assets/comilla_final.png';
+import { Image } from '../../Image';
+
+const LeftMark = styled(Image)`
+  position: absolute;
+  max-width: 10em;
+  max-height: 60%;
+  top: 0;
+  left: 2em;
 `;
 
-const Texto = styled(Text)`
-  line-height: 1.5em;
-  align-self: flex-end;
-  width: 70%;
+const RightMark = styled(Image)`
+  position: absolute;
+  max-width: 10em;
+  max-height: 50%;
+  bottom: 0;
+  right: 2em;
 `;
 
 const Quote = ({ author, children, size, ...props }) => (
-  <Container flex row backColor="green" {...props} justify="space-between">
-    <Author bold size={2 * size}>
-      {author}
-    </Author>
-    <Texto size={size} white align="right">
-      <Text as="span" white bold size={size * 1.05}>
-        "
+  <Container flex row backColor="green" {...props} padding={[2, 13]}>
+    <LeftMark src={inicial} />
+    <Container flex justify="space-between">
+      <Text white bold size={2 * size}>
+        {author}
       </Text>
-      {children}
-      <Text as="span" white bold size={size * 1.05}>
-        "
+      <Text size={size} white align="right">
+        {children}
       </Text>
-    </Texto>
+    </Container>
+    <RightMark src={final} />
   </Container>
 );
 
