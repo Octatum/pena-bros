a4fb4f98cb9ae7fc2d0c2fe628bf80ea40ecf23b
NissimBet <nissimbet123@outlook.com>
2018-11-12 11:02:44 -0600

    added services arrow scrolling functionality

--- src/components/Services/Presentation/index.jsx
+++ src/components/Services/Presentation/index.jsx
@@ -19,6 +19,22 @@ const ServiceNameColumn = styled(ServiceNames)`
   }
 `;
 
+const ViewComponent = styled(ServiceView)`
+  align-self: flex-start;
+`;
+
+const Arrow = styled(Container)`
+  width: 0;
+  height: 0;
+  left: -0.1em;
+  border-left: 0.9em solid transparent;
+  border-right: 0.9em solid transparent;
+
+  border-top: 1.5em solid white;
+
+  margin: auto;
+`;
+
 class ServicesPresentation extends Component {
   constructor(props) {
     super(props);
@@ -27,8 +43,13 @@ class ServicesPresentation extends Component {
       current: 0,
     };
 
-    this.names = props.data.map(data => data.node.frontmatter.name);
+    this.names = props.data.map(data => data.node.frontmatter.name)
+    this.hoverInterval = null;
+
     this.handleHover = this.handleHover.bind(this);
+    this.handleHoverScroll = this.handleHoverScroll.bind(this);
+    this.handleHoverClick = this.handleHoverClick.bind(this);
+    this.handleStopInterval = this.handleStopInterval.bind(this);
   }
 
   handleHover(event, index) {
@@ -37,19 +58,53 @@ class ServicesPresentation extends Component {
     });
   }
 
+  handleHoverScroll() {
+    this.hoverInterval = setInterval(() => {
+      const next = (this.state.current + 1) % this.names.length;
+      this.setState({
+        current: next,
+      });
+    }, 1000);
+  }
+
+  handleHoverClick() {
+    const next = (this.state.current + 1) % this.names.length;
+    this.setState({
+      current: next,
+    });
+  }
+
+  handleStopInterval() {
+    clearInterval(this.hoverInterval);
+  }
+
   render() {
     return (
       <Container flex row justify="flex-start" margin={[0, 0, 5, 0]}>
-        <ServiceNameColumn
-          handleHover={this.handleHover}
-          current={this.state.current}
-          names={this.names}
-          width="35%"
-          height="800px"
-        />
+        <Container width="30%">
+          <ServiceNameColumn
+            handleHover={this.handleHover}
+            current={this.state.current}
+            names={this.names}
+            height="800px"
+          />
+          {this.names.length > 5 ? (
+            <Container
+              padding={[1]}
+              backColor="black"
+              onMouseOver={this.handleHoverScroll}
+              onClick={this.handleHoverClick}
+              onMouseLeave={this.handleStopInterval}
+            >
+              <Arrow />
+            </Container>
+          ) : (
+            ''
+          )}
+        </Container>
         {this.props.data.map((data, index) => {
           return (
-            <ServiceView
+            <ViewComponent
               serviceData={data.node.frontmatter}
               height="800px"
               show={index === this.state.current}
