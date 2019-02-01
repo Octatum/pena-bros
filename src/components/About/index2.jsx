40d5e8dbeb50e19a3a24bbc2b08241628db7d566
NissimBet <nissimbet123@outlook.com>
2019-02-01 15:35:32 -0600

    added mobile version of about us page

--- src/components/About/index2.jsx
+++ src/components/About/index2.jsx
@@ -11,7 +11,7 @@ import SubTitle from '../SubTitle';
 const BackgroundContainer = styled(Container)`
   background-image: ${({ background }) => `url(${background})`};
   background-repeat: no-repeat;
-  background-size: 100% auto;
+  background-size: 100% 100%;
   background-position: center center;
 
   ::before {
@@ -51,15 +51,19 @@ const AboutUs = ({ className, ...props }) => (
       justify="center"
       align="flex-end"
       height="600px"
+      tHeight="400px"
       className={className}
       background={placeholder}
     >
       <Container
         flex
         width="50%"
+        tWidth="70%"
         height="auto"
         align="flex-end"
         padding={[0, 10]}
+        tPadding={[0]}
+        tMargin={['auto', 2.5,2.5,2.5]}
       >
         <Text white align="right" bold="bold" size={7}>
           About Us
@@ -71,7 +75,7 @@ const AboutUs = ({ className, ...props }) => (
       </Container>
     </BackgroundContainer>
 
-    <LeftEdgeContainer margin={[10, 'auto', 0, 5]} width="70%">
+    <LeftEdgeContainer margin={[10, 'auto', 0, 5]} tMargin={[0]} tMargin={[5, 2, 0, 2]} width="70%" tWidth="auto">
       <Text size={8} bold="bold">
         Lorem ipsum dolor sit amet.
       </Text>
@@ -81,7 +85,7 @@ const AboutUs = ({ className, ...props }) => (
       </Text>
     </LeftEdgeContainer>
 
-    <GreenBackground flex align="center" justify="center" padding={[5, 15, 2, 15]}>
+    <GreenBackground flex align="center" justify="center" padding={[5, 15, 2, 15]} tPadding={[5, 2.5]}>
       <Text size={7} white bold="lighter" align="center">
         <Text as="span" bold="bold" >Maecenas nec lacus </Text>
         mollis erat laoreet egestas. Morbi vitae blandit velit, at vehicula
@@ -92,9 +96,9 @@ const AboutUs = ({ className, ...props }) => (
       <Container width="5em" height="5px" backColor="white" margin={[2, 0]} />
     </GreenBackground>
 
-    <Image src={placeholder2} width="100%" height="700px" fit="cover" />
+    <Image src={placeholder2} width="100%" height="700px" tHeight="500px" fit="cover" />
 
-    <Container backColor="black" margin={[0, 10]} width="auto" style={{ top: '-3px' }} padding={[3, 5, 5, 5]}>
+    <Container backColor="black" margin={[0, 10]} tMargin={[0]} width="auto" style={{ top: '-3px' }} padding={[3, 5, 5, 5]} tPadding={[3.5, 2]}>
       <SubTitle size={3} white title="Lorem Ipsum">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi nostrum fuga inventore aliquam labore dolores, nihil accusantium expedita ratione non!</SubTitle>
     </Container>
   </Container>
