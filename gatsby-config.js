a8934a56ca4e85b4301d4fc3762905ef0a6c778c
Roberto Ruiz <rroberto.rruiz@gmail.com>
2019-05-13 18:19:57 -0500

    favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,20 @@ module.exports = {
     'gatsby-plugin-offline',
     'gatsby-plugin-netlify-cms',
     'gatsby-plugin-styled-components',
+
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Peña Bros`,
+        short_name: `pena-bros`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
+        display: 'standalone',
+        icon: `src/assets/pena-favicon.png`,
+        include_favicon: true, // Include favicon
+      },
+    },
     {
       resolve: 'gatsby-source-sanity',
       options: {
