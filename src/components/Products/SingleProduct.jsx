4c5c960e501edbb9774438d56fbda67f0540960e
Roberto Ruiz <rroberto.rruiz@gmail.com>
2019-07-10 12:41:24 -0500

    cambio de layout en productos

--- src/components/Products/SingleProduct.jsx
+++ src/components/Products/SingleProduct.jsx
@@ -1,51 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 import GatsbyImage from 'gatsby-image';
-
-import { Container } from '../Container';
-import SubTitle from '../SubTitle';
-import { device } from '../../utils/device';
-
-const SingleProdContainer = styled(Container)`
-  ${device.tablet} {
-    div {
-      color: black;
-    }
-  }
-`;
+import { Flex, Box } from 'rebass';
+import RebassText from '../RebassText';
 
 const IndivProd = ({ title, image, description, show, ...props }) => {
   return (
-    <SingleProdContainer flex show={show} {...props}>
-        <SubTitle
-          size={3}
-          title={title}
-          white
-          edgeColor="black"
-          tEdgeColor="green"
-          height="auto"
-          width="60%"
-          tWidth="90%"
-          margin={[4, 0]}
-          tMargin={[1, 1.4, 4, 'auto']}
+    <Flex
+      bg="green"
+      p={5}
+      flexDirection={['column', null, 'row']}
+      style={{ maxWidth: 1300 }}
+    >
+      <Box width={[1, null, 0.5]} px={3}>
+        <RebassText fontSize={6} fontWeight="bold" as="h2" color="white">
+          {title}
+        </RebassText>
+        <Box pl={2} mt={1} ml={1} style={{ borderLeft: '4px solid black' }}>
+          <RebassText lineHeight={2} color="white" fontSize={3}>
+            {description}
+          </RebassText>
+        </Box>
+      </Box>
+      <Box width={[1, null, 0.5]} px={3} pt={[4, null, 0]}>
+        <Flex
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          style={{ height: '100%' }}
         >
-          {description}
-        </SubTitle>
-      <Container
-        flex
-        backColor="transparent"
-        padding={[2, 8]}
-        tPadding={[0]}
-        width="95%"
-        tWidth="100%"
-        height="auto"
-        margin={[0, 0, 3, 'auto']}
-        tMargin={[0]}
-      >
-        <GatsbyImage style={{ width: "100%", maxWidth: '1000px' }} fluid={image.asset.fluid} />
-      </Container>
-    </SingleProdContainer>
+          <GatsbyImage
+            style={{ width: '100%', maxWidth: '1000px' }}
+            fluid={image.asset.fluid}
+          />
+        </Flex>
+      </Box>
+    </Flex>
   );
 };
 
